ae9db571004538ff17cb583f22506874f9e9a6ff
Frauke Von Der Haar <fvon-der@1-E-21.42heilbronn.de>
2024-03-06 22:29:19 +0100

    strlen OK

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,19 +6,19 @@
 /*   By: fvon-der <fvon-der@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 19:44:51 by fvon-der          #+#    #+#             */
-/*   Updated: 2024/03/06 19:44:58 by fvon-der         ###   ########.fr       */
+/*   Updated: 2024/03/06 19:57:33 by fvon-der         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strlen(char *str)
 {
-	int	count;
+	int	i;
 
-	count = 0;
-	while (*str != '\0')
+	i = 0;
+	while (*str)
 	{
-		count++;
+		i++;
 		str++;
 	}
-	return (count);
+	return (i);
 }
