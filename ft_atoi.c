52a87dcbcaba4159c35b97b5293e0c34eadbfce2
Frauke Von Der Haar <fvon-der@1-E-21.42heilbronn.de>
2024-03-06 22:36:01 +0100

    norminette kram

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: fvon-der <fvon-der@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 19:58:20 by fvon-der          #+#    #+#             */
-/*   Updated: 2024/03/06 22:24:46 by fvon-der         ###   ########.fr       */
+/*   Updated: 2024/03/06 22:35:15 by fvon-der         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,28 +20,22 @@ int	ft_atoi(char *str)
 {
 	int	sign;
 	int	result;
-	int	i;
 
 	sign = 1;
-	i = 0;
 	result = 0;
-	while (str[i] != '\0')
+	while (*str && ft_isspace(*str))
+		str++;
+	if (*str == '-')
 	{
-		while (ft_isspace(str[i]))
-			i++;
-		while (str[i] == '+' || str[i] == '-')
-		{
-			if (str[i] == '-')
-				sign *= -1;
-			i++;
-			break ;
-		}
-		while (str[i] >= '0' && str[i] <= '9')
-		{
-			result = (result * 10) + (str[i] - '0');
-			i++;
-		}
-		break ;
+		sign = -1;
+		str++;
+	}
+	else if (*str == '+')
+		str++;
+	while (*str != '\0' && *str >= '0' && *str <= '9')
+	{
+		result = (result * 10) + (*str - '0');
+		str++;
 	}
 	return (result * sign);
 }
