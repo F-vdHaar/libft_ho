686a96b65c473bb4cdedfa33e50825b229c12e60
Frauke Von Der Haar <fvon-der@1-D-16.42heilbronn.de>
2024-03-07 09:23:15 +0100

    basic skeleton

---
+++ ft_memmove.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fvon-der <fvon-der@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 07:46:31 by fvon-der          #+#    #+#             */
+/*   Updated: 2024/03/07 09:22:31 by fvon-der         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+//copies len bytes from string src to string dst.  The two strings may overlap;
+//the copy is always done in a non-destructive manner.
+//returns the original value of dst.
+void	*memmove(void *dst, const void *src, size_t len)
+{
+
+}
